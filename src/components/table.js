f6aac30152bade80ee79a0a6546228129086fbc3
TechesDe <57412420+TechesDe@users.noreply.github.com>
2025-04-19 19:00:47 +0200

    Добавлена тема для элементов

--- src/components/table.js
+++ src/components/table.js
@@ -9,7 +9,8 @@ import TablePagination from '@mui/material/TablePagination';
 import Paper from '@mui/material/Paper';
 import { Link } from '@mui/material';
 import * as locales from '@mui/material/locale';
-import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { theme as themeDefault } from '../theme/theme';
 
 function createData(number, name, importance, link, message) {
   return { number, name, importance, link, message };
@@ -24,11 +25,9 @@ function getRows(data){
 }
 
 export function BasicTable({id, data}) {
-    
-    const theme = useTheme();
-
 
     const locale = "ruRU";
+    const theme = themeDefault;
     const themeWithLocale = React.useMemo(
       () => createTheme(theme, locales[locale]),
       [locale, theme],
@@ -54,47 +53,49 @@ export function BasicTable({id, data}) {
   return (
     
       <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-        <TableContainer component={Paper}>
-          <Table size='small' sx={{ maxWidth: "100%" }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Номер</TableCell>
-                <TableCell>Название</TableCell>
-                <TableCell align="right">Важность</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {getRows(data[num]).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
-                <TableRow
-                  key={row.id}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.number}
-                  </TableCell>
-                  <TableCell align="right">
-                    <Link 
-                      href={row.link}
-                      sx={{ textAlign: 'left', display: '-webkit-box',  WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow:'clip', textOverflow: 'ellipsis' }}
-                    >{row.name}</Link>
-                    </TableCell>
-                  <TableCell align="right">{row.importance}</TableCell>
+        <ThemeProvider theme={themeDefault}>
+          <TableContainer component={Paper} theme = {themeDefault}>
+            <Table size='small' sx={{ maxWidth: "100%" }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Номер</TableCell>
+                  <TableCell>Название</TableCell>
+                  <TableCell align="right">Важность</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <ThemeProvider theme={themeWithLocale}>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              component="div"
-              count={getRows(data[num]).length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-          </ThemeProvider>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {getRows(data[num]).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+                  <TableRow
+                    key={row.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.number}
+                    </TableCell>
+                    <TableCell align="right">
+                      <Link 
+                        href={row.link}
+                        sx={{ textAlign: 'left', display: '-webkit-box',  WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow:'clip', textOverflow: 'ellipsis' }}
+                      >{row.name}</Link>
+                      </TableCell>
+                    <TableCell align="right">{row.importance}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <ThemeProvider theme={themeWithLocale}>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                count={getRows(data[num]).length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </ThemeProvider>
+          </TableContainer>
+        </ThemeProvider>
       </Paper>
   );
 }
